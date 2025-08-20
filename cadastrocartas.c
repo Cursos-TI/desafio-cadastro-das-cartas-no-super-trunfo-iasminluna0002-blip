a695af99b1bb4f328902c88f6c4e82ee61113f8c
iasminluna0002-blip <iasminluna0002@gmail.com>
2025-08-20 19:00:44 +0000

    cartas done

--- cadastrocartas.c
+++ cadastrocartas.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    // Declaração das variáveis da primeira carta
+    char codigo1[4]; // Ex: A01
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos_turisticos1;
 
-char codigo1[4];
-int populacao1;
-float area1;
-float pib1;
-int pontos_turisticos1;
-
-
-char codigo2[4]; // Ex: B02
+    // Declaração das variáveis da segunda carta
+    char codigo2[4]; // Ex: B02
     int populacao2;
     float area2;
     float pib2;
     int pontos_turisticos2;
 
+    // Cadastro da primeira carta
     printf("Cadastro da primeira carta:\n");
     printf("Digite o codigo da carta (ex: A01): ");
     scanf("%3s", codigo1);
@@ -31,6 +32,7 @@ char codigo2[4]; // Ex: B02
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &pontos_turisticos1);
 
+    // Cadastro da segunda carta
     printf("\nCadastro da segunda carta:\n");
     printf("Digite o codigo da carta (ex: B02): ");
     scanf("%3s", codigo2);
@@ -47,7 +49,8 @@ char codigo2[4]; // Ex: B02
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &pontos_turisticos2);
 
-     printf("\n=== Dados das Cartas ===\n");
+    // Exibição dos dados
+    printf("\n=== Dados das Cartas ===\n");
 
     printf("\nCarta 1 (%s):\n", codigo1);
     printf("Populacao: %d\n", populacao1);
